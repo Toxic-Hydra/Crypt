c3e3384881c5dd72d8a07bff947e626c20128618
Benjamin Chen <bchen42@ucsc.edu>
2019-05-16 17:48:24 -0700

    Added basic transition

--- phaser/Crypt/js/states/NewLife.js
+++ phaser/Crypt/js/states/NewLife.js
@@ -16,12 +16,14 @@ NewLife.prototype = {
         game.stage.backgroundColor = '#8594a3';
         var text = game.add.text(game.world.width / 2, 200, 'A new hero arises\n\nPress spacebar to enter the crypt', { fontSize: 24, fill: '#000', align: 'center' });
         text.anchor.set(0.5);
-        
+
         // Wait for spacebar press, then go to Play state
         var key = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         key.onDown.addOnce(this.startRun, this);
+
+				game.camera.flash('#000000', 1000);
     },
-    
+
     startRun: function()
     {
         game.state.start('play');
